7f9d7ac6c8ec06fd806050d0d475a1d359d7f339
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-06-22 13:52:08 -0300

    Ajustes pontuais na API de carteira do cliente

--- app/controllers/api/v1/client_wallets_controller.rb
+++ app/controllers/api/v1/client_wallets_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ClientWalletsController < Api::V1::ApiController
   def create
-    client_wallet_params = params.require(:client_wallet).permit(:registered_number, :email)
     client_wallet = ClientWallet.new(client_wallet_params)
     if client_wallet.save
         render status: 201, json: client_wallet
@@ -10,9 +9,18 @@ class Api::V1::ClientWalletsController < Api::V1::ApiController
   end
 
   def balance
-    wallet_registered_number = params.require(:client_wallet).permit(:registered_number)
-    client_wallet = ClientWallet.find_by(registered_number: wallet_registered_number[:registered_number]) 
+    client_wallet = ClientWallet.find_by(registered_number: wallet_registered_number_params[:registered_number]) 
     raise ActiveRecord::RecordNotFound if client_wallet.nil?
     render status: 200, json: client_wallet.as_json(except: [:category, :email])
+  end
+
+  private
+
+  def client_wallet_params
+    params.require(:client_wallet).permit(:registered_number, :email)
+  end
+
+  def wallet_registered_number_params
+    params.require(:client_wallet).permit(:registered_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'current_rate', to: 'exchanges_rate#current_rate'
-      resources :client_wallets, only: %i[create] do
-      end
+      resources :client_wallets, only: %i[create]
       get 'client_wallet/balance', to: 'client_wallets#balance'
     end
   end
