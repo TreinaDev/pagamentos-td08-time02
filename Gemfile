f10f69b643ed9b881ae809f999d96ce46a749c08
Luis Felipe <lfmarquesf@gmail.com>
2022-06-24 16:57:05 -0300

    Adiciona funcionalidade de envio de taxa ativa via API
    Co-authored-by: Lucas Marcelino <lucas-marcelino1@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+#webmock
+gem 'webmock'
+
 # Devise
 gem 'devise'
 
--- app/controllers/admin_backoffice_controller.rb
+++ app/controllers/admin_backoffice_controller.rb
@@ -1,3 +1,8 @@
 class AdminBackofficeController < ApplicationController
+  rescue_from Errno::ECONNREFUSED, with: :connection_refused
   before_action :authenticate_admin!
+
+  def connection_refused
+    flash[:notice] = 'Ação realizada com sucesso, porém a conexão com o e-commerce não ocorreu!'
+  end
 end
