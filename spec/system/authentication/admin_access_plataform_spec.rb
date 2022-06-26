458316e33f4190cf4412861882797b05a1915526
Wilian Ferreira <wpaife@gmail.com>
2022-06-26 13:13:45 -0300

    fix: Adiciona gem sassc-rails

--- spec/system/authentication/admin_access_plataform_spec.rb
+++ spec/system/authentication/admin_access_plataform_spec.rb
@@ -6,10 +6,10 @@ describe 'Administrador ativo acessa a plataforma' do
                           email: 'admin@userubis.com.br', status: :active)
 
     visit root_path
-    within('header') do 
+    within('header') do
      click_on 'Entrar'
     end
-    
+
     within('form') do
       fill_in 'E-mail', with: 'admin@userubis.com.br'
       fill_in 'Senha', with: '12345678'
@@ -23,7 +23,7 @@ describe 'Administrador ativo acessa a plataforma' do
   it 'e faz logout' do
     admin = Admin.create!(name: 'Fulano da Silva', password: '12345678', registration_number: '123.456.789-00',
                           email: 'admin@userubis.com.br', status: :active)
-    
+
     login_as(admin)
     visit root_path
     click_on 'Sair'
