57099f606cff8d4fa0391af70e2d6e91b656f966
Luis Felipe <lfmarquesf@gmail.com>
2022-06-30 18:42:47 -0300

    Arruma testes
    Co-authored by: Lucas Eduardo Campolino <campolino.l@gmail.com>

--- spec/system/credit_limit/admin_approve_and_reject_credit_spec.rb
+++ spec/system/credit_limit/admin_approve_and_reject_credit_spec.rb
@@ -9,12 +9,14 @@ describe 'Administrador acessa página de créditos pendentes' do
 
     login_as(admin)
     visit(root_path)
-    click_on('Créditos Pendentes')
+    click_on('Crédito Pendente')
 
-    expect(page).to have_content('Crédito pendente: R$ 200,00')
-    expect(page).to have_content('CPF: 111.111.111-11')
+    expect(page).to have_css 'th', text: 'CPF ou CNPJ'
+    expect(page).to have_css 'th', text: 'Valor'
+    expect(page).to have_css 'td', text: 'R$ 200,00'
+    expect(page).to have_css 'td', text: '111.111.111-11'
     expect(page).to have_button('Aprovar')
-    expect(page).to have_button('Rejeitar')
+    expect(page).to have_button('Recusar')
   end
 
   it 'aprova crédito com sucesso' do
@@ -25,7 +27,7 @@ describe 'Administrador acessa página de créditos pendentes' do
 
     login_as(admin)
     visit(root_path)
-    click_on('Créditos Pendentes')
+    click_on('Crédito Pendente')
     click_on('Aprovar')
 
     expect(page).to have_current_path(admin_backoffice_credits_path)
@@ -44,8 +46,8 @@ describe 'Administrador acessa página de créditos pendentes' do
 
     login_as(admin)
     visit(root_path)
-    click_on('Créditos Pendentes')
-    click_on('Rejeitar')
+    click_on('Crédito Pendente')
+    click_on('Recusar')
 
     expect(page).to have_current_path(admin_backoffice_credits_path)
     expect(page).to have_content('Não existem créditos pendentes no momento')
@@ -65,8 +67,8 @@ describe 'Administrador acessa página de créditos pendentes' do
 
     login_as(admin)
     visit root_path
-    click_on 'Créditos Pendentes'
-    click_on 'Aprovar'
+    click_on('Crédito Pendente')
+    click_on('Aprovar')
 
     expect(page).to have_current_path(admin_backoffice_credits_path)
     expect(page).to have_content('Não existem créditos pendentes no momento')
